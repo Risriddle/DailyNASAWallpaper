a88c77da26841358716fe6dcb057380705d3e0a2
Rishita Sharma <108649364+Risriddle@users.noreply.github.com>
2024-12-03 21:06:21 +0530

    Update set_wallpaper_on_login.sh

--- set_wallpaper_on_login.sh
+++ set_wallpaper_on_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source /home/risriddle/Workstation/Projects/Python/setWallpaper/myenv/bin/activate
+source /home/username/ProjectFolderName/envName/bin/activate
 
 # Run the Python script
-/home/risriddle/Workstation/Projects/Python/setWallpaper/myenv/bin/python /home/risriddle/Workstation/Projects/Python/setWallpaper/getSetWallpaper.py
+/home/username/projectFolderName/envName/bin/python /home/username/Workstation/ProjectFolderName/getSetWallpaper.py
