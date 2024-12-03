b8f2a492015c489fda6faa969c5314aa477dfc7c
Rishita Sharma <108649364+Risriddle@users.noreply.github.com>
2024-12-03 21:03:24 +0530

    Update getSetWallpaper.py

--- getSetWallpaper.py
+++ getSetWallpaper.py
@@ -1,46 +1,3 @@
-# import os
-# import requests
-# from dotenv import load_dotenv
-
-
-# load_dotenv(dotenv_path='/home/risriddle/Workstation/Projects/Python/setWallpaper/.env')
-
-# api_key = os.getenv('API_KEY')
-
-# res = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}")
-
-# if res.status_code == 200:
-#     data = res.json()
-#     image_url = data.get('url')
-
-#     if image_url:
-#         print(f"Image URL: {image_url}")
-
-#         image_response = requests.get(image_url)
-#         if image_response.status_code == 200:
-#             folder_path = "/home/risriddle/Pictures/NASA_APOD"
-
-#             os.makedirs(folder_path, exist_ok=True)
-
-#             image_extension = image_url.split('.')[-1]
-#             file_path = os.path.join(folder_path, f"apod.{image_extension}")
-
-#             with open(file_path, "wb") as file:
-#                 file.write(image_response.content)
-
-#             print(f"Image saved successfully to {file_path}")
-
-#             # Set wallpaper for dark mode using GNOME command
-#             os.system(f"gsettings set org.gnome.desktop.background picture-uri-dark file://{file_path}")
-#             print("Wallpaper set successfully in dark mode style.")
-#         else:
-#             print("Failed to fetch the image content.")
-#     else:
-#         print("No image found in the response.")
-# else:
-#     print(f"Error: Unable to fetch data, status code {res.status_code}")
-
-
 
 import os
 import requests
@@ -56,7 +13,7 @@ def is_connected():
     except OSError:
         return False
 
-load_dotenv(dotenv_path='/home/risriddle/Workstation/Projects/Python/setWallpaper/.env')
+load_dotenv(dotenv_path='/home/username/ProjectFolder/.env')
 
 # Check for internet connection up to 15 minutes (900 seconds)
 for _ in range(90):
@@ -73,7 +30,7 @@ for _ in range(90):
 
                 image_response = requests.get(image_url)
                 if image_response.status_code == 200:
-                    folder_path = "/home/risriddle/Pictures/NASA_APOD"
+                    folder_path = "/home/username/Pictures/NASA_APOD"
 
                     os.makedirs(folder_path, exist_ok=True)
 
